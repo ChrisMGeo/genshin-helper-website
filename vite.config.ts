785168f7ecf6e2e52ead56484963ddd0e0c49cb0
Chris George <chrismathew98@gmail.com>
2024-08-18 08:42:14 -0400

    Got rid of proxy to genshin.jmp

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/genshin-helper-website/',
-  server: {
-    cors: false,
-    proxy: {
-      '/api': {
-        target: 'https://genshin.jmp.blue/',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   plugins: [react()],
 })
